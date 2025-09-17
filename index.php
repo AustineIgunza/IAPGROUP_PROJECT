909c3f1417524a4ad25f3e53d588467efe88af95
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-09-17 09:39:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php
 require 'ClassAutoLoad.php';
 
-// Create instances (adjust class names if yours differ)
-$layout = new Layouts();
-$form   = new Forms();
-
-// If you have a Hello class in Global/Hello.php, instantiate it; otherwise skip.
-$hello = null;
-if (class_exists('Hello')) {
-    $hello = new Hello();
-}
-
-// Output the page
-echo $layout->header($conf);
-
-if ($hello) {
-    // If Hello::today() returns a string:
-    echo $hello->today();
-    // If it echoes directly, call: $hello->today();
-}
-
-//
-// Use signup() depending on how your Forms class is implemented:
-//
-// - If signup() RETURNS HTML (string): use echo $form->signup();
-// - If signup() ECHOES the HTML directly: use $form->signup();
-//
-echo $form->signup();
-
-echo $layout->footer($conf);
+$ObjLayout->header($conf);
+$ObjLayout->navbar($conf);
+$ObjLayout->banner($conf);
+$ObjLayout->content($conf);
+$ObjLayout->footer($conf);
